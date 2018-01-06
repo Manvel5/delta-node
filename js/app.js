eab1eb0d49c2fb22e868356267d39f20f18680f2
Manvel <manvel150@gmail.com>
2018-01-06 20:31:40 +0400

    add index.html

---
+++ js/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer(function(req, res) {
+	const content = fs.readFileSync('../index.html', 'utf8');
+	res.end(content);
+});
+
+server.listen(process.env.PORT || 3000);
