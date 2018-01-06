2d523d56dfe5ee6547dab2a792380307bbbedcba
Manvel <manvel150@gmail.com>
2018-01-06 20:48:38 +0400

    add package.json

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer(function(req, res) {
-	const content = fs.readFileSync('../index.html', 'utf8');
+	const content = fs.readFileSync('index.html', 'utf8');
 	res.end(content);
 });
 
